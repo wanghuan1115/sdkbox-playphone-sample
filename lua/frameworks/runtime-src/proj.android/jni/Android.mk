d876505852aae173cb838d6a60961672daceaef5
wanghuan <wanghuan1115@live.cn>
2015-11-26 10:57:35 +0800

    sdkbox update for lua 1.4.1.7

--- lua/frameworks/runtime-src/proj.android/jni/Android.mk
+++ lua/frameworks/runtime-src/proj.android/jni/Android.mk
@@ -14,6 +14,7 @@ FILE_LIST += $(wildcard $(LOCAL_PATH)/../../Classes/ide-support/*.c)
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 
 LOCAL_LDLIBS := -landroid -llog
+LOCAL_CPPFLAGS := -DSDKBOX_ENABLED
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 LOCAL_WHOLE_STATIC_LIBRARIES := PluginIAP sdkbox android_native_app_glue PluginAchievement PluginLeaderboard
 
