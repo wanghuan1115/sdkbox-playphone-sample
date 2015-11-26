fa263f51802964d87d1d5fcc6ae687cec77d3bc1
wanghuan <wanghuan1115@live.cn>
2015-11-26 10:54:01 +0800

    sdkbox update for js 1.4.1.7

--- js/frameworks/runtime-src/proj.android/jni/Android.mk
+++ js/frameworks/runtime-src/proj.android/jni/Android.mk
@@ -9,6 +9,7 @@ LOCAL_MODULE_FILENAME := libcocos2djs
 LOCAL_SRC_FILES := ../../Classes/AppDelegate.cpp ../../Classes/ide-support/SimpleConfigParser.cpp ../../Classes/ide-support/RuntimeJsImpl.cpp hellojavascript/main.cpp ../../Classes/PluginIAPJS.cpp ../../Classes/PluginIAPJSHelper.cpp ../../Classes/SDKBoxJSHelper.cpp ../../Classes/PluginAchievementJS.cpp ../../Classes/PluginAchievementJSHelper.cpp ../../Classes/PluginLeaderboardJS.cpp ../../Classes/PluginLeaderboardJSHelper.cpp
 
 LOCAL_LDLIBS := -landroid -llog
+LOCAL_CPPFLAGS := -DSDKBOX_ENABLED
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 LOCAL_WHOLE_STATIC_LIBRARIES := PluginIAP sdkbox android_native_app_glue PluginAchievement PluginLeaderboard
 
